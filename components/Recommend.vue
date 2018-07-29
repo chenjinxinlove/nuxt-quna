8b24741585b06f9ce02032cb60569127fcc212dc
chenjinxinlove <505150704@qq.com>
2018-07-29 18:29:11 +0800

    recommend

---
+++ components/Recommend.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="recommend">
+    <div class="title">热销推荐</div>
+    <ul>
+      <li class="item">
+        <div class="item-img-wrapper">
+          <img src="http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_150x150_06cb3e52.jpg" alt="">
+        </div>
+        <div class="item-info">
+          <p class="item-title">故宫</p>
+          <p class="item-desc">故宫故宫故宫故宫</p>
+          <div class="item-buttom">查看详情</div>
+        </div>
+      </li>
+      <li class="item">
+        <div class="item-img-wrapper">
+          <img src="http://img1.qunarzz.com/sight/p0/1708/2b/2b3b94de99c0a425a3.img.jpg_150x150_2dec3e78.jpg" alt="">
+        </div>
+        <div class="item-info">
+          <p class="item-title">八达岭长城</p>
+          <p class="item-desc">八达岭长城八达岭长城</p>
+          <div class="item-buttom">查看详情</div>
+        </div>
+      </li>
+      <li class="item">
+        <div class="item-img-wrapper">
+          <img src="http://img1.qunarzz.com/sight/p0/1505/d2/d274c92de14c93da.water.jpg_150x150_2747e538.jpg" alt="">
+        </div>
+        <div class="item-info">
+          <p class="item-title">颐和园</p>
+          <p class="item-desc">颐和园颐和园颐和园</p>
+          <div class="item-buttom">查看详情</div>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Recommend'
+}
+</script>
+<style lang="stylus" scoped>
+@import '../assets/stylus/varibles.styl'
+.recommend
+  .title
+    line-height 80px
+    background #eee
+    text-indent 20px
+  .item
+    display flex
+    overflow hidden
+    border-bottom 1px solide #ccc
+    img
+      width 170px
+      height 170px
+      padding 10px
+    .item-info
+      flex 4
+      padding 10px
+      .item-title
+        line-height 54px
+        font-size 32px
+      .item-desc
+        line-height 32px
+        font-size 28px
+        color #ccc
+        overflow hidden
+        text-overflow ellipsis
+        white-space nowrap
+      .item-buttom
+        display inline-block
+        line-height 50px
+        background #ff9300
+        padding 0 20px
+        border-radius 3px
+        margin-top 20px
+        color #fff
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -3,18 +3,21 @@
     <HomeHeader />
     <HomeSwiper />
     <HomeIcons />
+    <HomeRecommend />
   </div>
 </template>
 
 <script>
 import HomeHeader from '~/components/Header.vue'
 import HomeSwiper from '~/components/Swiper.vue'
-import HomeIcons from "~/components/Icons.vue";
+import HomeIcons from "~/components/Icons.vue"
+import HomeRecommend from '~/components/Recommend.vue'
 export default {
   components: {
     HomeHeader,
     HomeSwiper,
-    HomeIcons
+    HomeIcons,
+    HomeRecommend
   }
 }
 </script>
