6cdbaea3ff155a8948eb841652dd1851dc1e094e
陈金鑫 <505150704@qq.com>
2018-07-30 17:55:52 +0800

    list

---
+++ components/City/List.vue
@@ -0,0 +1,132 @@
+<template>
+  <div class="list">
+    <div class="area">
+      <div class="title border-topbottom">当前城市</div>
+      <div class="buttom-list">
+        <div class="buttom-wrapper">
+          <div class="buttom">北京</div>
+        </div>
+        <div class="buttom-wrapper">
+          <div class="buttom">北京</div>
+        </div>
+        <div class="buttom-wrapper">
+          <div class="buttom">北京</div>
+        </div>
+        <div class="buttom-wrapper">
+          <div class="buttom">北京</div>
+        </div>
+        <div class="buttom-wrapper">
+          <div class="buttom">北京</div>
+        </div>
+        <div class="buttom-wrapper">
+          <div class="buttom">北京</div>
+        </div>
+        <div class="buttom-wrapper">
+          <div class="buttom">北京</div>
+        </div>
+      </div>
+    </div>
+    <div class="area">
+      <div class="title border-topbottom">热门城市</div>
+      <div class="buttom-list">
+        <div class="buttom-wrapper">
+          <div class="buttom">北京</div>
+        </div>
+        <div class="buttom-wrapper">
+          <div class="buttom">北京</div>
+        </div>
+        <div class="buttom-wrapper">
+          <div class="buttom">北京</div>
+        </div>
+        <div class="buttom-wrapper">
+          <div class="buttom">北京</div>
+        </div>
+        <div class="buttom-wrapper">
+          <div class="buttom">北京</div>
+        </div>
+        <div class="buttom-wrapper">
+          <div class="buttom">北京</div>
+        </div>
+        <div class="buttom-wrapper">
+          <div class="buttom">北京</div>
+        </div>
+      </div>
+    </div>
+    <div class="area">
+      <div class="title border-topbottom">A</div>
+      <div class="item-list">
+        <div class="item border-bottom">
+          阿卡索
+        </div>
+        <div class="item border-bottom">
+          阿卡索
+        </div>
+        <div class="item border-bottom">
+          阿卡索
+        </div>
+        <div class="item border-bottom">
+          阿卡索
+        </div>
+        <div class="item border-bottom">
+          阿卡索
+        </div>
+        <div class="item border-bottom">
+          阿卡索
+        </div>
+        <div class="item border-bottom">
+          阿卡索
+        </div>
+        <div class="item border-bottom">
+          阿卡索
+        </div>
+        <div class="item border-bottom">
+          阿卡索
+        </div>
+        <div class="item border-bottom">
+          阿卡索
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'CityList'
+}
+</script>
+<style lang="stylus" scoped>
+@import '../../assets/stylus/varibles.styl'
+.list
+  margin-top 10px
+  overflow hidden
+  position absolute
+  left 0
+  right 0
+  top 158px
+  right 0
+  .border-topbottom, .border-bottom
+    border 1px slide #ccc
+  .title
+    line-height 40px
+    background #eeeeee
+    padding-left 20px
+    color #666
+    font-size 26px
+  .buttom-list
+    padding 10px 60px 10px 10px
+    overflow hidden
+    .buttom-wrapper
+      float left
+      width 33.33%
+      .buttom
+        padding 10px 0
+        text-align center
+        margin 10px
+        border 2px solid #ccc
+        border-radius 6px
+  .item-list
+    .item
+      line-height 76px
+      padding-left 20px
+</style>
+
--- pages/city/index.vue
+++ pages/city/index.vue
@@ -2,16 +2,19 @@
   <div>
       <CityHeader />
       <CitySearch />
+      <CityList />
   </div>
 </template>
 
 <script>
 import CityHeader from '~/components/City/Header.vue'
 import CitySearch from '~/components/City/Search.vue'
+import CityList from '~/components/City/List.vue'
 export default {
   components: {
     CityHeader,
-    CitySearch
+    CitySearch,
+    CityList
   },
 }
 </script>
