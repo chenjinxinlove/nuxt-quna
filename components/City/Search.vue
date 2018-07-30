1bbe4a663d28cc08bbb85db10b4038a082e54c14
陈金鑫 <505150704@qq.com>
2018-07-30 17:04:57 +0800

    search

---
+++ components/City/Search.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="search">
+   <input type="text" placeholder="输入城市名或拼音" class="search-input">
+  </div>
+</template>
+<script>
+export default {
+  name: 'CitySearch'
+}
+</script>
+<style lang="stylus" scoped>
+@import '../../assets/stylus/varibles.styl'
+.search
+  height 72px
+  padding 10px
+  background $bgColor
+  .search-input
+    box-sizing border-box
+    border-style none
+    outline none
+    width 100%
+    height 62px
+    padding 0 10px
+    line-height 62px
+    text-align center
+    border-radius 6px
+    color #666
+</style>
+
--- pages/city/index.vue
+++ pages/city/index.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
       <CityHeader />
+      <CitySearch />
   </div>
 </template>
 
 <script>
 import CityHeader from '~/components/City/Header.vue'
+import CitySearch from '~/components/City/Search.vue'
 export default {
   components: {
-    CityHeader
+    CityHeader,
+    CitySearch
   },
 }
 </script>
